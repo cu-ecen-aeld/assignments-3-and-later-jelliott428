5e4182c207c04cdd889bd23bca8b612826d8c0ea
jone <jon.elliott428@gmail.com>
2023-09-18 17:47:36 -0500

    Completed assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,13 @@
 #include "systemcalls.h"
 
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -9,15 +17,8 @@
 */
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+	int ret = system( cmd ); 
+    return ret >= 0;
 }
 
 /**
@@ -33,7 +34,11 @@ bool do_system(const char *cmd)
 *   fork, waitpid, or execv() command, or if a non-zero return value was returned
 *   by the command issued in @param arguments with the specified arguments.
 */
-
+/*
+    TEST_ASSERT_FALSE_MESSAGE(do_exec(3, "/usr/bin/test","-f","echo"),
+             "The exec() function should have returned false since echo was not specified"
+             " with absolute path in argument to the test executable.");
+*/
 bool do_exec(int count, ...)
 {
     va_list args;
@@ -47,21 +52,48 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
 /*
- * TODO:
+ * DONE:
  *   Execute a system command by calling fork, execv(),
  *   and wait instead of system (see LSP page 161).
  *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
+ *   (first argument to execv), ***and use the remaining arguments
+ *   as second argument to the execv() command***
+ *
+ *   JDE NOTE: The highlighted instruction is INCORRECT. Passing &command[1] as the 2nd argument,
+ * 	 which is what the instruction is saying, does not work. Instead must pass 'command'
+ *	 ( the address of the command[] array )!!! Applied same change to do_exec_redirect()).
+ *	 WTF!?!?!? 
  *
 */
+	pid_t pid;
+	bool result = false;
+	
+	// In the child, successful invocation of fork() returns '0'.
+	// In the parent, it returns the child's pid.	
+	pid = fork();
+	if( pid != -1 )
+	{
+		if( pid == 0 )
+		{
+			// Child process
+			execv( command[0], command );	// changes the child to run the command with args
+			exit( EXIT_FAILURE );	// only time execv() returns is if there is an error in child process creation
+		}
+		
+		int status;
+		if( waitpid( pid, &status, 0 ) != -1 )
+		{
+			// check that the child's return status was '0' (OK)
+			if( WIFEXITED( status ) && WEXITSTATUS( status ) == 0 ) 
+				result = true;
+		}
+	}
 
     va_end(args);
-
-    return true;
+    return result;
 }
 
 /**
@@ -82,18 +114,50 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
-
+    // command[count] = command[count];
 
 /*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+ * DONE
+ *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a reference,
  *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
+ *   The rest of the behavior is same as do_exec()
  *
 */
-
+	pid_t pid;
+	bool result = false;
+	
+	int fd = open( outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644 );
+	if( fd >= 0 )
+	{
+		// In the child, successful invocation of fork() returns '0'.
+		// In the parent, it returns the child's pid.	
+		pid = fork();
+		if( pid != -1 )
+		{
+			if( pid == 0 )
+			{
+				// Child process
+				if( dup2( fd, STDOUT_FILENO ) >= 0 )	// dup2 redirects stdout to outputfile 
+				{
+					close( fd );						// closes child's extra file descriptor (?)
+					execv( command[0], command );		// changes the child to run the command with args
+				}
+				exit( EXIT_FAILURE );	// only time execv() returns is if there is an error in child process creation
+			}
+			
+			int status;
+			if( waitpid( pid, &status, 0 ) != -1 )
+			{
+				// check that the child's return status was '0' (OK)
+				if( WIFEXITED( status ) && WEXITSTATUS( status ) == 0 ) 
+				result = true;
+			}
+		}
+		
+		// always close the opened file
+		close( fd );
+	}
+	
     va_end(args);
-
-    return true;
+    return result;
 }
